c65522c1e0281c636a112b5b6336352d758ea1b5
paoloangeles <paolo.angeles09@gmail.com>
2020-05-20 08:34:22 +0100

    Added init, play and chooseaction functions

--- pathfinder_RL.py
+++ pathfinder_RL.py
@@ -5,6 +5,8 @@ Created on Tue May 19 10:25:37 2020
 @author: Paolo
 """
 
+## Import required modules
+import numpy as np
 
 ## Define board characteristics
 
@@ -15,6 +17,22 @@ win_state = [3, 2]
 lose_state = [3, 0]
 blockade = [1, 1]
 deterministic = True
+
+
+
+# Define initial characteristics of agent
+
+def __init__(self):
+    
+    # how quickly the agent learns
+    self.learning_rate = 0.1
+    
+    # the possible moves of the agent
+    self.actions = ["up", "down", "left", "right"]
+
+    # if agent finds a path to win state, should it stick to it?
+    # define agent's willingness to explore outside of best reward state path
+    self.exploration_rate = 0.5
 
 
 ## Define reward states
@@ -31,6 +49,8 @@ def get_reward(self):
 ## Define agent possible moves
 
 def next_state_move(self, action):
+    
+    # define potential moves of agent
     if self.determine:
         if action == "up":
             next_state = (self.state[0] - 1, self.state[1])
@@ -43,10 +63,69 @@ def next_state_move(self, action):
         else:
             print("Illegal move made by agent")
         
-        # if move is legal
+        # decide if move is legal and output
         if (next_state[0] >= 0) and (next_state[0] <= 2):
             if (next_state[1] >= 0) and (next_state[0] <= 3):
                 if next_state != blockade:
                     return next_state
         
-        return self.state # if move is illegal
+        return self.state # output if move is illegal
+    
+def play(self, iterations = 10):
+    
+    # begin game
+    i = 0
+    
+    while i < iterations:
+    
+        # check if agent has reached the end state (NEED TO DO)
+        if self.State.isEnd:
+            
+            # begin back propagation process
+            reward = self.get_reward()
+            
+    
+            # define value iteration formula
+            for s in reversed(self.states):
+                # define separate state values (estimated rewards)
+                self.state_values[s] = self.state_values[s] + learning_rate*(reward - self.state_values[s])
+            
+            # reset agent states (but not state values!)
+            self.reset()
+            
+            # begin next iteration
+            i += 1
+        
+        # agent is stil playing game so needs to decide next action
+        else:
+            # decide on action based on estimated reward
+            action = self.choose_action()
+            
+            
+
+
+def choose_action(self):
+    
+    
+    if np.random.uniform(0, 1) <= self.exploration_rate:
+        # agent is going to explore
+        action = np.random.choice(self.actions)
+    else:
+        # agent is going to stick to optimal reward path
+        
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
