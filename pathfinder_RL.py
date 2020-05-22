aa7bd76dbd9671c66a9475733cd3ca82e5af4ac1
paoloangeles <paolo.angeles09@gmail.com>
2020-05-22 17:18:23 +0100

    Finished choose action and play function

--- pathfinder_RL.py
+++ pathfinder_RL.py
@@ -34,6 +34,11 @@ def __init__(self):
     # define agent's willingness to explore outside of best reward state path
     self.exploration_rate = 0.5
 
+    # initialise state reward values
+    self.state_values = {}
+    for row in range(board_rows):
+        for col in range(board_columns):
+            self.state_values[row, col] = 0
 
 ## Define reward states
 
@@ -48,7 +53,7 @@ def get_reward(self):
     
 ## Define agent possible moves
 
-def next_state_move(self, action):
+def next_state_position(self, action):
     
     # define potential moves of agent
     if self.determine:
@@ -67,9 +72,9 @@ def next_state_move(self, action):
         if (next_state[0] >= 0) and (next_state[0] <= 2):
             if (next_state[1] >= 0) and (next_state[0] <= 3):
                 if next_state != blockade:
-                    return next_state
+                    return next_state # return next state position
         
-        return self.state # output if move is illegal
+        return self.state # output state position if move is illegal
     
 def play(self, iterations = 10):
     
@@ -88,7 +93,7 @@ def play(self, iterations = 10):
             # define value iteration formula
             for s in reversed(self.states):
                 # define separate state values (estimated rewards)
-                self.state_values[s] = self.state_values[s] + learning_rate*(reward - self.state_values[s])
+                self.state_values[s] = self.state_values[s] + self.learning_rate*(reward - self.state_values[s])
             
             # reset agent states (but not state values!)
             self.reset()
@@ -100,20 +105,28 @@ def play(self, iterations = 10):
         else:
             # decide on action based on estimated reward
             action = self.choose_action()
-            
+            self.state = self.next_state_position(action)
             
 
 
 def choose_action(self):
     
+    action = ""
     
     if np.random.uniform(0, 1) <= self.exploration_rate:
         # agent is going to explore
         action = np.random.choice(self.actions)
     else:
         # agent is going to stick to optimal reward path
-        
+        current_reward = 0
+        for action_option in self.actions: # cycle through action options
+            next_reward = self.state_values[self.next_state_position[action_option]] # check rewards of each state action
             
+            if next_reward > current_reward:
+                action = action_option
+                current_reward = next_reward
+            
+    return action
             
             
             
